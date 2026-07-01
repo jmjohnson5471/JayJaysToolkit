ce058b1298b65c7d1a26fd46e076def9c377d724
jmjohnson5471 <73309421+jmjohnson5471@users.noreply.github.com>
2026-07-01 14:20:32 -0400

    Delete Plugins/WindowsDeployment directory

--- Plugins/WindowsDeployment/Tools/Source/Open Local Generator Folder.ps1
+++
@@ -1,6 +0,0 @@
-$base = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)))
-$folder = Join-Path $base "LocalGenerator"
-New-Item -ItemType Directory -Force -Path $folder | Out-Null
-Start-Process explorer.exe $folder
-Write-Host "Opened LocalGenerator folder:"
-Write-Host $folder
